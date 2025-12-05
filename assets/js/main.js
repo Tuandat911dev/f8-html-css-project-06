e14eb0a2c9c0d85e82520ca9bd3e47aa4148ca7f
PhamTuanDat <pdat100111@gmail.com>
2025-12-05 14:58:19 +0700

    feat: faq section

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,46 @@ thumbVideoEl.addEventListener("click", (e) => {
     videoEl.play();
   }
 });
+
+// navigation
+const navigateItemEl = document.querySelectorAll(".glide__bullet");
+
+navigateItemEl.forEach((item) => {
+  item.addEventListener("click", () => {
+    const activeNavEl = document.querySelector(".glide__bullet--active");
+    activeNavEl.classList.remove("glide__bullet--active");
+    item.classList.add("glide__bullet--active");
+  });
+});
+
+//  FAQ
+const faqEl = document.querySelectorAll(".faq-item");
+
+faqEl.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    const answer = faq.querySelector(".faq-item__answer");
+    const faqBtnEl = faq.querySelector(".faq-item__btn");
+
+    if (faq.classList.contains("active")) {
+      answer.style.height = answer.scrollHeight + "px";
+      requestAnimationFrame(() => {
+        answer.style.height = "0px";
+      });
+
+      faq.classList.remove("active");
+    } else {
+      faq.classList.add("active");
+      answer.style.height = answer.scrollHeight + "px";
+
+      answer.addEventListener(
+        "transitionend",
+        () => {
+          if (faq.classList.contains("active")) {
+            answer.style.height = "auto";
+          }
+        },
+        { once: true }
+      );
+    }
+  });
+});
